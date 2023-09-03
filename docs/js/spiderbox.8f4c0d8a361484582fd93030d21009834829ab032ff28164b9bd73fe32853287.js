c2db06fc0f26301bc7037d9cda5580b7fc75ae9e
TRHX <admin@itbob.cn>
2023-09-03 14:08:17 +0800

    submit 2023.09.03

---
+++ docs/js/spiderbox.8f4c0d8a361484582fd93030d21009834829ab032ff28164b9bd73fe32853287.js
@@ -0,0 +1,251 @@
+/* 控制台输出 */
+function makeMulti(string) {
+    let l = new String(string)
+    l = l.substring(l.indexOf("/*") + 3, l.lastIndexOf("*/"))
+    return "%c " + l
+};
+const string = function () {
+    /*
+               _____       _     __          ____
+              / ___/____  (_)___/ /__  _____/ __ )____  _  __        爬虫爬得欢
+              \__ \/ __ \/ / __  / _ \/ ___/ __  / __ \| |/_/       监狱要坐穿
+             ___/ / /_/ / / /_/ /  __/ /  / /_/ / /_/ />  <        数据玩得溜
+            /____/ .___/_/\__,_/\___/_/  /_____/\____/_/|_|       牢饭吃个够
+                /_/
+    */
+};
+console.log(makeMulti(string), "color: #0084ff");
+console.log("\n %c © BOB'S BLOG %c itbob.cn %c © SpiderBox %c spiderbox.cn %c © WebStack %c webstack.cc \n", "color: #fadfa3; background: #030307; padding:5px 0;", "background: #fadfa3; padding:5px 0;", "color: #ffffff; background: #0084ff; padding:5px 0;", "background: #fadfa3; padding:5px 0;", "color: #ffffff; background: #f1404b; padding:5px 0;", "background: #fadfa3; padding:5px 0;");
+
+/* 弹窗一：say hello baby */
+$(document).ready(function () {
+    // 检查本地存储中的标志位，如果标志位为 true，则不再弹出弹窗
+    const shouldShowPopup = localStorage.getItem("shouldShowPopup") !== "false";
+    if (shouldShowPopup) {
+        Swal.fire({
+            // width: 300,
+            title: "欢迎",
+            text: "扫码关注站长公众号：爬虫矛与盾",
+            showCancelButton: true,
+            confirmButtonText: "俺知道了",
+            cancelButtonText: "不再弹出",
+            confirmButtonColor: "#0084ff",
+            imageUrl: "/images/qrcode.jpg",
+            // imageWidth: 300,
+            // imageHeight: 300,
+            imageAlt: "公众号：爬虫矛与盾",
+            showClass: {
+                popup: "swal2-show"
+            },
+            hideClass: {
+                popup: "swal2-hide"
+            }
+        }).then((result) => {
+            if (result.isConfirmed) {} else {
+                localStorage.setItem("shouldShowPopup", "false");
+            }
+        });
+        const popupContainer = document.querySelector('.swal2-container');
+        popupContainer.style.zIndex = '9999';
+    }
+});
+
+/* 弹窗二：交流群 */
+$(document).ready(function () {
+    const showGroupElement = document.getElementById("show-group");
+    showGroupElement.addEventListener("click", function () {
+        Swal.fire({
+            text: "扫码加入QQ/微信交流群",
+            confirmButtonText: "俺知道了",
+            confirmButtonColor: "#0084ff",
+            imageWidth: 420,
+            imageUrl: "/images/group.png",
+            imageAlt: "QQ/微信交流群",
+            showClass: {
+                popup: "swal2-show"
+            },
+            hideClass: {
+                popup: "swal2-hide"
+            },
+            allowOutsideClick: false,
+            customClass: {
+                image: "custom-swal-image"
+            }
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.close();
+            }
+        });
+        const popupContainer = document.querySelector(".swal2-container");
+        popupContainer.style.zIndex = "9999";
+    })
+});
+
+/* 弹窗三：免责声明 */
+$(document).ready(function () {
+    let popupWidth = "55%"; // 默认宽度
+    if (window.innerWidth < 768) {
+        popupWidth = "95%";
+    } else {
+        popupWidth = "55%";
+    }
+
+    const showDisclaimerElement = document.getElementById("show-disclaimer");
+    showDisclaimerElement.addEventListener("click", function () {
+        Swal.fire({
+            html: `
+            <div class="custom-swal-container">
+                <h3>免责声明</h3>
+                <p style="text-align: left;">欢迎使用我们的导航站 [SpiderBox 虫盒]！请在使用本站点之前仔细阅读以下免责声明，要访问和使用本站点，您必须接受以下条款和条件！如果您不同意这些条款和条件，请勿使用本站点！</p>
+                <ol class="custom-swal-ol">
+                    <li>
+                        <span>信息准确性</span>
+                        <p>本站点提供的导航链接和信息仅供参考，尽管我们努力确保信息的准确性和完整性，但我们不对信息的准确性、完整性、实时性或适用性作出任何明示或暗示的陈述或担保，您应该自行核实和验证您所获得的信息！</p>
+                    </li>
+                    <li>
+                        <span>使用风险</span>
+                        <p>使用本站点提供的导航链接和信息是基于您自己的判断和风险，我们不对您使用本站点导航链接导致的任何损失、损害或不便负责，在使用导航链接之前，请确保您已经充分了解所涉及的风险！</p>
+                    </li>
+                    <li>
+                        <span>第三方链接</span>
+                        <p>本站点包含的所有指向第三方网站的链接仅供您的方便提供，不代表我们对这些第三方网站的认可或担保，我们对这些第三方网站的内容、隐私政策和行为不承担任何责任，您在访问这些链接时需谨慎自负风险！</p>
+                    </li>
+                    <li>
+                        <span>知识产权和版权</span>
+                        <p>本站点为非营利性网站，尊重知识产权和版权，基于互联网之分享精神，如果您认为本站点包含侵犯您的知识产权或版权的内容，请与我们联系，我们会尽快删除或更正相关内容！</p>
+                    </li>
+                    <li>
+                        <span>法律合规</span>
+                        <p>您使用本站点的行为应遵守适用的法律法规，您对任何违反法律法规的行为负有全部责任，我们不对您的任何违法行为负责！</p>
+                    </li>
+                    <li>
+                        <span>免责声明的变更</span>
+                        <p>我们保留随时修改或更新本免责声明的权利，请您定期查阅本页面以获取最新信息！</p>
+                    </li>
+                  </ol>
+    
+                  <h3>联系方式</h3>
+                  <ol class="custom-swal-ol">
+                    <li>
+                        <span>站长邮箱：</span>
+                        <p style="display: inline;margin: 0;" id="show-email">点击获取</p>
+                    </li>
+                    <li>
+                        <span>站长公众号：</span>爬虫矛与盾
+                    </li>
+                  </ol>
+                  <p class="custom-swal-date">2023年08月17日</p>
+            </div>
+            `,
+            width: popupWidth,
+            showCancelButton: true,
+            confirmButtonText: "同意",
+            cancelButtonText: "不同意",
+            confirmButtonColor: "#0084ff",
+            showClass: {
+                popup: "swal2-show"
+            },
+            hideClass: {
+                popup: "swal2-hide"
+            },
+            allowOutsideClick: false,
+            customClass: {
+                // popup: "custom-swal-popup"
+                container: "custom-swal-container"
+            }
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.close();
+            } else if (result.dismiss === Swal.DismissReason.cancel) {
+                window.close();
+            }
+        });
+        const popupContainer = document.querySelector(".swal2-container");
+        popupContainer.style.zIndex = "9999";
+
+        var showEmailElement = document.getElementById("show-email");
+        showEmailElement.addEventListener("click", function () {
+            showEmailElement.innerHTML = atob("YWRtaW5AaXRib2IuY24=");
+        });
+    });
+});
+
+/* 弹窗三：打赏赞助 */
+$(document).ready(function () {
+    const showSponsorElement = document.getElementById("show-sponsor");
+    showSponsorElement.addEventListener("click", function () {
+        Swal.fire({
+            text: "所有打赏、赞助将用于 SpiderBox 的域名、服务器、COS 等项目续费。",
+            confirmButtonText: "俺知道了",
+            confirmButtonColor: "#0084ff",
+            imageWidth: 420,
+            imageUrl: "/images/sponsor.png",
+            imageAlt: "微信/支付宝收款码",
+            showClass: {
+                popup: "swal2-show"
+            },
+            hideClass: {
+                popup: "swal2-hide"
+            },
+            allowOutsideClick: false,
+            customClass: {
+                image: "custom-swal-image"
+            }
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.close();
+            }
+        });
+
+        const popupContainer = document.querySelector(".swal2-container");
+        popupContainer.style.zIndex = "9999";
+    })
+});
+
+/* 夜间(日间)模式 */
+(function () {
+    if (document.cookie.replace(/(?:(?:^|.*;\s*)night\s*\=\s*([^;]*).*$)|^.*$/, "$1") === '') {
+        if (new Date().getHours() > 22 || new Date().getHours() < 6) {
+            document.body.classList.remove('io-black-mode');
+            document.body.classList.add('io-grey-mode');
+            document.cookie = "night=1;path=/";
+            console.log('夜间模式开启');
+        } else {
+            document.body.classList.remove('night');
+            document.cookie = "night=0;path=/";
+            console.log('夜间模式关闭');
+        }
+    } else {
+        var night = document.cookie.replace(/(?:(?:^|.*;\s*)night\s*\=\s*([^;]*).*$)|^.*$/, "$1") || '0';
+        if (night == '0') {
+            document.body.classList.remove('night');
+        } else if (night == '1') {
+            document.body.classList.add('night');
+        }
+    }
+})();
+
+/* 夜间(日间)模式切换 */
+// $("#search-bg").css("background-image", "url({{ $.Site.Params.images.searchImageL }})");   //默认浅色背景
+function switchNightMode() {
+    var night = document.cookie.replace(/(?:(?:^|.*;\s*)night\s*\=\s*([^;]*).*$)|^.*$/, "$1") || '0';
+    if (night == '0') {
+        // $("#search-bg").css("background-image", "url({{ $.Site.Params.images.searchImageL }})");
+        document.body.classList.remove('io-grey-mode');
+        document.body.classList.add('io-black-mode');
+        document.cookie = "night=1;path=/"
+        console.log(' ');
+        $(".switch-dark-mode").attr("data-original-title", "日间模式");
+        $(".mode-ico").removeClass("icon-night");
+        $(".mode-ico").addClass("icon-light");
+    } else {
+        // $("#search-bg").css("background-image", "url({{ $.Site.Params.images.searchImageD }})")
+        document.body.classList.remove('io-black-mode');
+        document.body.classList.add('io-grey-mode');
+        document.cookie = "night=0;path=/"
+        console.log(' ');
+        $(".switch-dark-mode").attr("data-original-title", "夜间模式");
+        $(".mode-ico").removeClass("icon-light");
+        $(".mode-ico").addClass("icon-night");
+    }
+}
